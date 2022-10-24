f76464640eb6b7a8c7d91bfd9773b16a2fd61e01
parveen6010 <parveenprajapati6010@gmail.com>
2022-10-25 03:04:05 +0530

    int

--- script.js
+++ script.js
@@ -15,3 +15,56 @@ if(close){
     
     
 }
+const studentForm = document.getElementById("studentForm");
+const studentsContainer = document.querySelector(".students");
+const nameInput = studentForm["name"];
+const ageInput = studentForm["age"];
+const rollInput = studentForm["roll"];
+
+const students =  JSON.parse(localStorage.getItem("students")) ||  [];
+      
+const addstudent = (name , age , roll) => {
+
+    students.push({
+        name: name,
+        age : age,
+        roll : roll,
+    })
+     localStorage.setItem("students",JSON.stringify(students));
+
+    return{name , age , roll};
+};
+
+
+ const createStudentElement = ({name , age , roll}) => {
+    const studentDiv = document.createElement("div");
+    const studentName = document.createElement("h2"); 
+    const studentAge = document.createElement("p"); 
+    const studentRoll = document.createElement("p"); 
+
+    studentName.innerText = " Name:- " + name;
+    studentAge.innerText = "Email:- " + age;
+    studentRoll.innerText = "Message:- " + roll;
+
+    studentDiv.append(studentName, studentAge, studentRoll);
+    studentsContainer.appendChild(studentDiv);
+
+    studentsContainer.style.display = students.length == 0 ? "none" : "flex";
+ };
+
+
+ students.forEach(createStudentElement);
+
+ studentForm.onsubmit = ()=>{
+    
+    const newstudent = addstudent(nameInput.value,ageInput.value,rollInput.value);
+
+     createStudentElement(newstudent);
+
+     nameInput.value = "";
+     ageInput.value = "";
+     rollInput.value = "";
+ }
+
+
+  
